6ef18d3d40f1f67b8588ad033cc0a90aaee37961
uj-python <ujwalshekar.c@gmail.com>
2022-01-24 07:32:36 +0530

    serializers fields to all

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,16 +1,16 @@
-from rest_framework import routers, serializers, viewsets
 from django.contrib.auth.models import User
+from rest_framework import  serializers
 from .models import CustomUser,UserRegistration
 
 
 
 class UserSerializers(serializers.ModelSerializer):
-    print("------here in the serializers class---------")
+ 
     class Meta:
-        print("------inside the meta ---------")
+      
         model = CustomUser
-        fields = '__all__'
-        # fields = ('username', 'email')
+        
+        fields = ('id','username', 'email')
 
 
 class RegisteredSerializres(serializers.ModelSerializer):
